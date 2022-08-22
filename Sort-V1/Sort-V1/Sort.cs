efdb75042a5a2a4f3377bde4453742d4ae934d90
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-22 12:19:25 +0430

    Add files via upload

---
+++ Sort-V1/Sort-V1/Sort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_V1
+{
+    internal class Sort
+    {
+        public void Sorting(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
